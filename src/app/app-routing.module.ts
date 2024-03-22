04c4c57afaf28526e6897fc29be4978d84d2648d
Federico Fernandez <134979454+FernandezFederico@users.noreply.github.com>
2024-03-22 20:22:05 -0300

    feat(routing): Configuracion de rutas en forRoot\n\n- Agrego rutas basicas, que luego seran mejoradas con lazy loading

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,15 +2,36 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { NotFoundComponent } from './modules/not-found/not-found.component';
 import { DashboardComponent } from './modules/dashboard/dashboard.component';
+import { HomeComponent } from './modules/dashboard/pages/home/home.component';
+import { UsersComponent } from './modules/dashboard/pages/users/users.component';
 
 
 const routes: Routes = [
   {
-    path: '',
+    path: 'dashboard',
     component: DashboardComponent,
+    children: [
+      {
+        path: 'home',
+        component: HomeComponent,
+        
+      },
+      {
+        path: 'users',
+        component: UsersComponent,
+      },
+      {
+        path: '**',
+        redirectTo: 'home',
+      },
+    ]
   },
   {
     path: '**',
+    redirectTo: 'dashboard/home',
+  },
+  {
+    path: '404',
     component: NotFoundComponent,
   },
 
